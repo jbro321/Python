4564467dabe47e232374d6cbb0725b776c4ce330
jbro321 <jaehyung0321@gmail.com>
2021-05-05 21:33:18 +0900

    210505

--- Python/Fastcampus_Python/Python_Basic/section02_1.py
+++ Python/Fastcampus_Python/Python_Basic/section02_1.py
@@ -33,3 +33,11 @@ print("%s's foavorite number is %d" % ('JP', 7))
 
 print("Test1: %5d, Price: %4.2f" % (776, 6534.123)) # %4.2f는 정수부분 4자리, 실수부분 2자리
 print("Test1: {0: 5d}, Price:{1: 4.2f}".format(776, 6534.123)) # {0: 5d}는 0번째는 5d이다
+print("Test1: {a: 5d}, Price:{b: 4.2f}".format(a=776, b=6534.123))
+
+print("'you'")
+print("\'you\'")
+print('"you"')
+print("""'you'""")
+print('\\you\\\n\n\n\n')
+print('\t\t\ttest')
