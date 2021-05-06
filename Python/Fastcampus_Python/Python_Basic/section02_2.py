9206bf6a41940ee580327c7b99d8795687d0168f
jbro321 <jaehyung0321@gmail.com>
2021-05-06 21:49:05 +0900

    210506

--- Python/Fastcampus_Python/Python_Basic/section02_2.py
+++ Python/Fastcampus_Python/Python_Basic/section02_2.py
@@ -17,9 +17,38 @@ myName = 'Goodboy'
 
 # 조건문
 if myName == "Goodboy":
-    print('Ok')
+    print('Ok') # indent도 문법이다
 
 # 반복문
 for i in range(1, 10):
     for j in range(1, 10):
         print('%d * %d = ' % (i, j), i*j)
+
+# 변수 선언(한글)
+
+이름 = "좋은사람"
+
+# 출력
+print(이름)
+
+# 함수 선언
+
+def 인사():
+    print("안녕하세요. 반갑습니다.")
+
+인사()
+
+def greeting():
+    print('Hello!')
+
+greeting()
+
+# 클래스
+class Cookie(): # class Cookie: 괄호 없어도 가능하다.
+    pass
+
+# 객체 생성
+cookie = Cookie()
+
+print(id(cookie))
+print(dir(cookie))
