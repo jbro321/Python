a6a318294252f6eb8d072a040766cc895f9e6df4
jbro321 <jaehyung0321@gmail.com>
2021-05-07 23:21:11 +0900

    210507

---
+++ Python/Fastcampus_Python/Python_Basic/section04_1.py
@@ -0,0 +1,26 @@
+# section04_1
+# 데이터 타입
+
+v_str1 = "Niceman"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name" : "kim",
+    "age" : 25
+}
+
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7, 8, 9} # 집합
+
+print(type(v_str1))
+print(type(v_bool))
+print(type(v_str2))
+print(type(v_bool))
+print(type(v_float))
+print(type(v_int))
+print(type(v_dict))
+print(type(v_tuple))
+print(type(v_set))
