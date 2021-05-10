d0d0a49de895b5680c5736d0a76a2beda87bc0a5
jbro321 <jaehyung0321@gmail.com>
2021-05-10 09:57:53 +0900

    210510

--- Python/Fastcampus_Python/Python_Basic/section04_3.py
+++ Python/Fastcampus_Python/Python_Basic/section04_3.py
@@ -22,3 +22,53 @@ print(e[-1][-2])
 # 슬라이싱
 print(d[0:2])
 print(e[2][1:3])
+
+# 연산
+print(c + d)
+print(c * 3)
+print(str(c[0])+'hi')
+
+# 리스트 수정, 삭제
+c[0] = 77
+print(c)
+
+c[1:2] = [100, 1000, 10000] # c의 1과 2사이
+print(c)
+c[1] = ['a', 'b', 'c']
+print(c)
+
+del c[1]
+print(c)
+del c[-1]
+print(c)
+print()
+print()
+
+
+# 리스트 함수
+y = [5, 2, 3, 1, 4]
+print(y)
+y.append(6)
+print(y)
+y.sort() # 오름차순
+print(y)
+y.reverse() # 내림차순
+print(y)
+y.insert(2, 7)
+print(y)
+y.remove(2) # 숫자 2를 찾아서 지운다.
+y.remove(7)
+print(y)
+y.pop() # 맨 마지막을 꺼내고 없앤다. 스택이다.
+print(y) # LIFO
+ex = [88, 77]
+y.extend(ex)
+print(y)
+y.append(ex) # ex가 리스트라서 리스트 자체를 삽입
+print(y)
+
+# 삭제 : del, remove, pop
+
+ 
+
+
