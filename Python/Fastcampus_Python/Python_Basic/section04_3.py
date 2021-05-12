825bb2efdd32f5474e5d57c0e9fe725bc6df6654
jbro321 <jaehyung0321@gmail.com>
2021-05-13 07:31:44 +0900

    210513

--- Python/Fastcampus_Python/Python_Basic/section04_3.py
+++ Python/Fastcampus_Python/Python_Basic/section04_3.py
@@ -71,4 +71,35 @@ print(y)
 
 # 튜플 (순서o, 중복o, 수정x, 삭제x)
 
+a = ()
+b = (1, )
+c = (1, 2, 3, 4)
+d = (10, 100, ('a', 'b', 'c'))
+
+# del c[2] 튜플은 삭제할 수 없다.
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2:1])
+print(d[2][0:2])
+
+print(c + d)
+print(c * 3)
+print()
+print()
+print()
+
+# 튜플 함수
+
+z = (5, 2, 1, 3, 4)
+
+print(z)
+print(3 in z)
+print(z.index(5))
+print(z.count(1))
+
+
+
 
