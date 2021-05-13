5aef19b92c1c7818945f104911c3cef1be8cad7c
jbro321 <jaehyung0321@gmail.com>
2021-05-13 18:55:54 +0900

    210513

---
+++ Python/Fastcampus_Python/Python_Basic/section05_2.py
@@ -0,0 +1,34 @@
+# section05_2
+# 파이썬 흐름제어(반복문)
+# 반복문 실습
+
+# 코딩의 핵심 -> 조건 해결 중요
+
+# 기본 반복문 : for, while
+
+v1 = 1
+while v1 < 11:
+    print("v1 is :", v1)
+    v1 += 1
+
+for v2 in range(10):
+    print("v2 is :", v2)
+
+for v3 in range(1, 10):
+    print("v3 is :", v3)
+
+# 1 ~ 100 합
+
+sum1 = 0
+cnt1 = 0
+
+while cnt1 <= 100:
+    sum1 += cnt1
+    cnt1 += 1
+
+print('1 ~ 100 : ', sum1)
+print('1 ~ 100 : ', sum(range(1, 101)))
+print('1 ~ 100 : ', sum(range(1, 101, 2)))
+
+
+
